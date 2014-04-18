c99acdadbcb757cc7e6d7e512360e3e5914b4897
Veera Sekhar <complanboy2@gmail.com>
2014-04-18 15:35:22 +0530

    Update Test.java

--- RestaurantPuzzle/kitchen_src/com/restaurant/io/Test.java
+++ RestaurantPuzzle/kitchen_src/com/restaurant/io/Test.java
@@ -12,7 +12,7 @@ import com.restaurant.utils.Utils;
 /**
  * this class helps in testing the application
  * 
- * @author <a href="mailto:sekhar@gmail.com">G.V.Sekhar</a>
+ * @author <a href="mailto:complanboy2@gmail.com">G.V.Sekhar</a>
  * @version $Revision$, $Date$, $Author$.
  * @since April 11, 2014
  */
